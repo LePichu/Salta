c0b92d004081681b52c9a234393a98fcf2305d9d
LePichu <ishat.prsnl65844@gmail.com>
2023-01-23 14:49:24 +0530

    feat: fix vendoring script; add next release metadata json

--- dart-sass/vendor.ps1
+++ dart-sass/vendor.ps1
@@ -3,14 +3,13 @@ $global:ProgressPreference = "SilentlyContinue"
 $SASS_PATH = "./dart-sass/dart-sass.tgz"
 $SASS_VER = "1.57.1"
 
-if (Test-Path $SASS_PATH) {
-    Remove-Item $SASS_PATH
-}
+Remove-Item $SASS_PATH -Force -ErrorAction SilentlyContinue
+Remove-Item "./dart-sass/package" -Force -Recurse -ErrorAction SilentlyContinue
 
 Invoke-WebRequest -Uri "https://registry.npmjs.org/@lumeland/sass/-/sass-$SASS_VER.tgz" -Outfile $SASS_PATH
 tar -xf $SASS_PATH -C "./dart-sass"
 Remove-Item $SASS_PATH
 
-Set-Location "$SASS_PATH/dart-sass"
+Set-Location "./dart-sass/package"
 npm install
 Set-Location "../../"
